c6090b2d201cb98e013d739bc40bc516d89fd5a8
kuu(Fumiya Kume) <fumiya.kume@hotmail.com>
2021-12-28 12:54:45 +0900

    Setup homebrew

--- setup.sh
+++ setup.sh
@@ -6,17 +6,7 @@ sh jobs/setup_xcode_install.sh > /dev/null
 # Homebrew
 
 echo "✅ Install Homebrew"
-
-# Setup HomeBrew
-ln -sf "$(pwd)/Brewfile" ~
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh) > /dev/null" 
-
-echo "✅ Setup Homebrew bundle"
-brew tap homebrew/bundle
-
-echo "✅ Bundle Install"
-brew update > /dev/null
-brew bundle > /dev/null
+sh jobs/setup_homebrew.sh > /dev/null
 
 echo /usr/local/bin/fish | sudo tee -a /etc/shells 2>&1
 sudo chsh -s /usr/local/bin/fish > /dev/null
