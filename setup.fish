9eac5bee55ed4d10714e37266715e5043eaab3cf
kuu(Fumiya Kume) <fumiya.kume@hotmail.com>
2021-12-28 12:52:16 +0900

    Extract config files copy task

--- setup.fish
+++ setup.fish
@@ -1,27 +1,11 @@
 #!/bin/fish
 
-# config
-mkdir ~/.config
-ln -s "$pwd/.config" ~/.config
-
-# Git
-
-assert_has git
-ln -s "$pwd/.gitconfig" ~/.gitconfig
-
-# Tig
-
-assert_has tig
-ln -s "$pwd/.tigrc" ~/.tigrc
-
-# VSCode
-
-ln -sf "$pwd/vscode/User/" ~/Library/Application\ Support/Code/
-
 # XCode
-
 xcode-select --install
 sudo xcodebuild -license
+
+# Copy config files
+fish jobs/setup_config_files_copy.fish > /dev/null
 
 # Mac Defaults
 fish jobs/setup_mac_defaults.fish > /dev/null
