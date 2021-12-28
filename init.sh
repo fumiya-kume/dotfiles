58f6fc41860c64451d33dc73c8f40a2b4ce2c0db
kuu(Fumiya Kume) <fumiya.kume@hotmail.com>
2021-12-28 23:33:14 +0900

    combine script (#14)

--- init.sh
+++ init.sh
@@ -1,2 +1,18 @@
-sh setup.sh
-fish setup.fish
+#!/bin/zsh
+
+echo "Install Xcode"
+sh jobs/setup_xcode_install.sh > /dev/null
+
+# Homebrew
+
+echo "✅ Install Homebrew"
+sh jobs/setup_homebrew.sh > /dev/null
+
+echo /usr/local/bin/fish | sudo tee -a /etc/shells 2>&1
+sudo chsh -s /usr/local/bin/fish > /dev/null
+
+# Copy config files
+fish jobs/setup_config_files_copy.fish > /dev/null
+
+# Mac Defaults
+fish jobs/setup_mac_defaults.fish > /dev/null
--- setup.fish
+++
@@ -1,7 +0,0 @@
-#!/bin/fish
-
-# Copy config files
-fish jobs/setup_config_files_copy.fish > /dev/null
-
-# Mac Defaults
-fish jobs/setup_mac_defaults.fish > /dev/null
