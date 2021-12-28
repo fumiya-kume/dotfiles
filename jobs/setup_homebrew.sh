122ffbd3fb884091ab57a669ded8c81e8805548a
kuu(Fumiya Kume) <fumiya.kume@hotmail.com>
2021-12-28 23:28:56 +0900

    supress

--- jobs/setup_homebrew.sh
+++ jobs/setup_homebrew.sh
@@ -1,6 +1,6 @@
 # Setup HomeBrew
 ln -sf "$(pwd)/Brewfile" ~
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh) > /dev/null" 
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" > /dev/null 
 
 echo "✅ Setup Homebrew bundle"
 brew tap homebrew/bundle
