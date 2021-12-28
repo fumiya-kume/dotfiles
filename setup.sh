1e236a0992bfaab26cfd9a1bda122ca1ce8bf29d
kuu(Fumiya Kume) <fumiya.kume@hotmail.com>
2021-12-28 12:06:42 +0900

    Fix messages

--- setup.sh
+++ setup.sh
@@ -1,26 +1,21 @@
 #!/bin/zsh
 
-# sudo -v
-
 # Setup HomeBrew
 ln -sf "$(pwd)/Brewfile" ~
 
 # Homebrew
 
-echo "Install Homebrew"
+echo "✅ Install Homebrew"
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh) > /dev/null" 
 
-echo "Setup Homebrew bundle"
-
+echo "✅ Setup Homebrew bundle"
 brew tap homebrew/bundle
 
-echo "Bundle Install"
-
+echo "✅ Bundle Install"
 brew update > /dev/null
 brew bundle > /dev/null
 
 echo /usr/local/bin/fish | sudo tee -a /etc/shells 2>&1
-
 sudo chsh -s /usr/local/bin/fish > /dev/null
 fish setup.fish
