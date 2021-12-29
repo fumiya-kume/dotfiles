f6186a6163d187ef7f143dbab5669a4b0deeac44
kuu(Fumiya Kume) <fumiya.kume@hotmail.com>
2021-12-29 12:49:02 +0900

    Refactoring and adding util methods

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,7 +7,4 @@ set PATH $HOME/.jenv/bin $PATH
 # ~/.anyenv/bin/anyenv init - fish | source
 status --is-interactive; and source (jenv init -|psub)
 
-abbr -a gcb git checkout -b
-abbr -a ts tig status
-
 starship init fish | source
--- .config/fish/functions/methods.fish
+++ .config/fish/functions/methods.fish
@@ -22,6 +22,10 @@ function g
    cd (ghq root)/(ghq list | peco)
 end
 
+function gp
+   git pull
+end
+
 function git-branch-remove
   git branch | peco | xargs git branch -D
 end
@@ -30,7 +34,14 @@ function bgr
    git branch | grep -v "master" | xargs git branch -D
 end
 
-
 function gadd
    git diff --name-only | peco | xargs git add
+end
+
+function ts
+   tig status
+end
+
+function gcb
+   git checkout -b $argv
 end
