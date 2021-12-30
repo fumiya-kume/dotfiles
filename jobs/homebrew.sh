8e16c7b4769301049b41823b99a4af4e10da9a85
kuu(Fumiya Kume) <fumiya.kume@hotmail.com>
2021-12-30 09:41:12 +0900

    remove brew cleanup

--- jobs/homebrew.sh
+++ jobs/homebrew.sh
@@ -10,8 +10,6 @@ brew update > /dev/null
 echo "✅ Brew upgrade"
 brew upgrade > /dev/null
 brew bundle > /dev/null
-echo "✅ Brew cleanup"
-brew cleanup > /dev/null
 echo "✅ Bundle Install"
 brew bundle
 brew bundle
