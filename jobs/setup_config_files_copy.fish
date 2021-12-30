bd0260299bed170686c031cd63212edd5688005c
kuu(Fumiya Kume) <fumiya.kume@hotmail.com>
2021-12-30 13:03:59 +0900

    add print for ssh key for github

--- jobs/setup_config_files_copy.fish
+++ jobs/setup_config_files_copy.fish
@@ -11,6 +11,11 @@ echo "✅ Generate SSH key for Github"
 ln -s "$pwd/.ssh" ~/.ssh
 ssh-keygen -t rsa -f ~/.ssh/github -N '' -q
 
+echo "SSH key for Github"
+echo "------------------"
+pbcopy < ~/.ssh/github.pub && pbpaste
+echo "------------------"
+
 # Tig
 assert_has tig
 ln -s "$pwd/.tigrc" ~/.tigrc
