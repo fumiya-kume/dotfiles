7cef5c69c6b1a7d1925fd4992588aca80d51e4c5
kuu(Fumiya Kume) <fumiya.kume@hotmail.com>
2021-12-30 09:41:50 +0900

    refactoring

--- jobs/homebrew.sh
+++ jobs/homebrew.sh
@@ -5,11 +5,7 @@ ln -sf "$(pwd)/Brewfile" ~
 echo "✅ Setup Homebrew bundle"
 brew tap homebrew/bundle
 
-echo "✅ Brew update"
-brew update > /dev/null
-echo "✅ Brew upgrade"
-brew upgrade > /dev/null
-brew bundle > /dev/null
+echo "✅ Brew update and upgrade"
+brew update > /dev/null && brew upgrade > /dev/null
 echo "✅ Bundle Install"
-brew bundle
-brew bundle
+brew bundle > /dev/null
