f777b51e6faebc993973415d1b6890abad2fed70
kuu(Fumiya Kume) <fumiya.kume@hotmail.com>
2021-12-30 17:36:53 +0900

    fix

--- init.sh
+++ init.sh
@@ -5,10 +5,10 @@ git clone https://github.com/fumiya-kume/dotfiles.git ~/dotfiles -q
 cd ~/dotfiles
 
 echo "✅ Install Xcode"
-sh jobs/install_xcode.sh
+. jobs/install_xcode.sh
 
 echo "✅ Install Homebrew"
-sh jobs/setup_homebrew.sh
+. jobs/setup_homebrew.sh
 
 echo "✅ Set fish as default shell"
 if [[ `uname -m` == 'arm64' ]]; then
@@ -18,14 +18,12 @@ else
    echo /usr/local/bin/fish | sudo tee -a /etc/shells 2>&1
    sudo chsh -s /usr/local/bin/fish > /dev/null
 fi
-fish
-cd ~/dotfiles
 
 echo "✅ Copy files"
-fish jobs/setup_config_files_copy.fish
+fish cd ~/dotfiles && fish jobs/setup_config_files_copy.fish
 
 echo "✅ Set Mac defaults"
-fish jobs/setup_mac_defaults.fish
+fish cd ~/dotfiles && fish jobs/setup_mac_defaults.fish
 
 echo "✅ Setup SSH for Github"
-fish jobs/setup_ssh_github.fish
+fish cd ~/dotfiles && fish jobs/setup_ssh_github.fish
