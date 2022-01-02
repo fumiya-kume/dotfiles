595850f4943c12bf1a350c7b64e1a37224990d08
kuu(Fumiya Kume) <fumiya.kume@hotmail.com>
2022-01-02 11:00:28 +0900

    add print for github gpg

--- config/.config/fish/functions/print_env.fish
+++ config/.config/fish/functions/print_env.fish
@@ -5,5 +5,6 @@ function print_env
     echo "------------------"
     echo "GPG"
     set KEY_ID (gpg --list-secret-keys --with-colons | awk -F: '$1 == "sec" {print $5}' | tail -n 1)
+    echo $KEY_ID | xargs gpg --armor --export 
     echo "git config --global user.signingkey $KEY_ID"
 end
