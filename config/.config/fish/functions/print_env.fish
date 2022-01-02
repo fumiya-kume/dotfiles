657f63de58e49e03913244918591f1e30dd3e492
kuu(Fumiya Kume) <fumiya.kume@hotmail.com>
2022-01-02 10:57:53 +0900

    fix for fish

--- config/.config/fish/functions/print_env.fish
+++ config/.config/fish/functions/print_env.fish
@@ -4,6 +4,6 @@ function print_env
     pbcopy < ~/.ssh/github.pub && pbpaste
     echo "------------------"
     echo "GPG"
-    KEY_ID=(gpg --list-secret-keys --with-colons | awk -F: '$1 == "sec" {print $5}' | tail -n 1)
+    set KEY_ID (gpg --list-secret-keys --with-colons | awk -F: '$1 == "sec" {print $5}' | tail -n 1)
     echo "git config --global user.signingkey $KEY_ID"
 end
