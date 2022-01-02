2cf038f1671db52b5ba9b3b9cd59055d59247ad7
kuu(Fumiya Kume) <fumiya.kume@hotmail.com>
2022-01-02 09:05:35 +0900

    restore necessary output

--- jobs/setup_homebrew.sh
+++ jobs/setup_homebrew.sh
@@ -1,4 +1,4 @@
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" > /dev/null
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 brew tap homebrew/bundle > /dev/null
 brew upgrade > /dev/null
