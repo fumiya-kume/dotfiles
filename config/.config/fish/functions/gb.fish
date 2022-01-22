e160ae3cd8b4de2eb73923356ff403d1c430cf3e
fumiya-kume <fumiya.kume@hotmail.com>
2022-01-22 15:21:21 +0900

    Update method of change the branch

--- config/.config/fish/functions/gb.fish
+++ config/.config/fish/functions/gb.fish
@@ -1,3 +1,3 @@
 function gb
-   git branch -a --sort=-authordate | grep -v -e '->' -e '*' | perl -pe 's/^\h+//g' | perl -pe 's#^remotes/origin/###' | perl -nle 'print if !$c{$_}++' | peco | xargs git checkout
+   git branch -a --sort=-authordate | grep -v -e '->' -e '*' | perl -pe 's/^\h+//g' | perl -pe 's#^remotes/origin/###' | perl -nle 'print if !$c{$_}++' | peco | xargs git switch -m
 end
