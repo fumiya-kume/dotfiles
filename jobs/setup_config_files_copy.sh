c28177decd9509bfaecff634d7628ba8bb6e2fc5
fumiya-kume <fumiya.kume@hotmail.com>
2022-03-18 01:12:36 +0900

    Update Brewfile config

--- jobs/setup_config_files_copy.sh
+++ jobs/setup_config_files_copy.sh
@@ -5,7 +5,7 @@ ln -s "$(pwd)/config/.gitconfig" ~/.gitconfig 2> /dev/null
 ln -fs "$(pwd)/config/.tigrc" ~/.tigrc
 mkdir -p "~/Library/Application\ Support/Code/"
 ln -nfs "$(pwd)/config/vscode/User/" "~/Library/Application\ Support/Code/"
-ln -s "$(pwd)/config/.Brewfile" ~/.Brewfile
+ln -s "$(pwd)/config/Brewfile" ~/Brewfile
 ln -s "$(pwd)/config/.hammerspoon" ~/.hammerspoon
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
