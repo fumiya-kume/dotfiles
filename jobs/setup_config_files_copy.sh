443a3c3441c2a1941fb9b6f42e38e293a1562f24
fumiya-kume <fumiya.kume@hotmail.com>
2022-03-18 21:28:40 +0900

    Add tmux setup config

--- jobs/setup_config_files_copy.sh
+++ jobs/setup_config_files_copy.sh
@@ -7,6 +7,7 @@ mkdir -p "~/Library/Application\ Support/Code/"
 ln -nfs "$(pwd)/config/vscode/User/" "~/Library/Application\ Support/Code/"
 ln -s "$(pwd)/config/Brewfile" ~/Brewfile
 ln -s "$(pwd)/config/.hammerspoon" ~/.hammerspoon
+ln -s "$(pwd)/config/.tmux/" ~/.tmux
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' > /dev/null
