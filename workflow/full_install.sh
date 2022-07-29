0fc7d2f550d26a5d5be8ebe4a1f5cea29a855f90
kuu(Fumiya Kume) <fumiya.kume@hotmail.com>
2022-07-29 22:14:03 +0900

    Update full_install.sh

--- workflow/full_install.sh
+++ workflow/full_install.sh
@@ -2,29 +2,31 @@ BASE=pwd
 BASE_CONFIG="$BASE/config"
 JOB_PATH="$BASE/jobs"
 
+cd ~/ghq/dotfiles
+
 echo "✅ Install Xcode"
-../jobs/install_xcode.sh
+./jobs/install_xcode.sh
 
 echo "✅ Copy files"
-../jobs/setup_config_files_copy.sh
+./jobs/setup_config_files_copy.sh
 
 echo "✅ Install Homebrew"
-../jobs/setup_homebrew.sh
+./jobs/setup_homebrew.sh
 
 echo "✅ Set fish as default shell"
-../jobs/setup_fish.sh
+./jobs/setup_fish.sh
 
 echo "✅ Set Mac defaults"
-../jobs/setup_mac_defaults.sh
+./jobs/setup_mac_defaults.sh
 
 echo "✅ Setup gn"
-../jobs/setup_gn.sh
+./jobs/setup_gn.sh
 
 echo "✅ Setup SSH for Github"
-../jobs/setup_ssh_github.sh
+./jobs/setup_ssh_github.sh
 
 echo "✅ Setup GPG for Github"
-../jobs/setup_gpg.sh
+./jobs/setup_gpg.sh
 
 echo "✅ Test configuration"
-../jobs/test.sh
+./jobs/test.sh
