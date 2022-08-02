7b2d00ac78b5d9136250c9efc196a959824e4194
kuu(Fumiya Kume) <fumiya.kume@hotmail.com>
2022-08-02 17:59:34 +0900

    Try use Gitconfig include

--- jobs/setup_config_files_copy.sh
+++ jobs/setup_config_files_copy.sh
@@ -1,13 +1,14 @@
 rm -rf ~/.config
 ln -s -f "$(pwd)/config/.config/" ~
 ln -s -f "$(pwd)/config/.gnupg/" ~
-ln -fs "$(pwd)/config/.gitconfig" ~/.gitconfig
 ln -fs "$(pwd)/config/.tigrc" ~/.tigrc
 mkdir -p "~/Library/Application\ Support/Code/"
 ln -nfs "$(pwd)/config/vscode/User/" "~/Library/Application\ Support/Code/"
 ln -s "$(pwd)/config/.Brewfile" ~/.Brewfile
 ln -s "$(pwd)/config/.hammerspoon" ~/.hammerspoon
 ln -s "$(pwd)/config/.tmux/" ~/.tmux
+
+git config --global include.path "$(pwd)/config/.gitconfig"
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' > /dev/null
