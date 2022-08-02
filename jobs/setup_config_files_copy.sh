631d2a51615fd5c01199aef506647dcaefd5ef28
kuu(Fumiya Kume) <fumiya.kume@hotmail.com>
2022-08-02 13:25:14 +0900

    Update copy for .gitignore

--- jobs/setup_config_files_copy.sh
+++ jobs/setup_config_files_copy.sh
@@ -1,7 +1,7 @@
 rm -rf ~/.config
 ln -s -f "$(pwd)/config/.config/" ~
 ln -s -f "$(pwd)/config/.gnupg/" ~
-ln -s "$(pwd)/config/.gitconfig" ~/.gitconfig 2> /dev/null
+ln -fs "$(pwd)/config/.gitconfig" ~/.gitconfig
 ln -fs "$(pwd)/config/.tigrc" ~/.tigrc
 mkdir -p "~/Library/Application\ Support/Code/"
 ln -nfs "$(pwd)/config/vscode/User/" "~/Library/Application\ Support/Code/"
