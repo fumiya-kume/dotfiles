01658c6537b24ef04938f11aa0773c597c220816
kuu(Fumiya Kume) <fumiya.kume@hotmail.com>
2022-08-03 22:33:15 +0900

    Refactoring Homebrew install (#42)

--- jobs/setup_homebrew.sh
+++ jobs/setup_homebrew.sh
@@ -1,5 +1,2 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-brew tap homebrew/bundle > /dev/null
-brew upgrade > /dev/null
-brew bundle install --file ~/.Brewfile > /dev/null
+brew tap homebrew/bundle > /dev/null && brew upgrade > /dev/null && brew bundle install --global
