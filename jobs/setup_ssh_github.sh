2b6666700146fb6f03def76e69c84eb73686c613
kuu(Fumiya Kume) <fumiya.kume@hotmail.com>
2022-08-05 08:38:19 +0900

    Update emoji for Github SSH generate (#60)

--- jobs/setup_ssh_github.sh
+++ jobs/setup_ssh_github.sh
@@ -1,5 +1,5 @@
 if [ -f "$1" ]; then
-  echo "  ✅ Generate SSH key for Github skipped"
+  echo "  🚀 Generate SSH key for Github skipped"
 else 
   echo "SSH key for Github"
   ssh-keygen -t ed25519 -f ~/.ssh/github -N '' -C "fumiya.kume@hotmail.com" -q > /dev/null
