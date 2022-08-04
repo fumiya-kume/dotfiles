548ff3b7081158ef917c27e308c4c5a5465da6e5
kuu(Fumiya Kume) <fumiya.kume@hotmail.com>
2022-08-04 22:08:54 +0900

    Update setup_ssh_github.sh (#50)
    * Update setup_ssh_github.sh
    
    * Update setup_config_files_copy.sh

--- jobs/setup_ssh_github.sh
+++ jobs/setup_ssh_github.sh
@@ -1,8 +1,7 @@
-rm -rf ~/.ssh 2> /dev/null
-ln -s "$(pwd)/config/.ssh/" ~/.ssh
-ssh-keygen -t ed25519 -f ~/.ssh/github -N '' -C "fumiya.kume@hotmail.com" -q > /dev/null
-
-echo "SSH key for Github"
-echo "------------------"
-pbcopy < ~/.ssh/github.pub && pbpaste
-echo "------------------"
+if [ -f "$1" ]; then
+  echo "  ✅ Generate SSH key for Github skipped"
+else 
+  echo "SSH key for Github"
+  ssh-keygen -t ed25519 -f ~/.ssh/github -N '' -C "fumiya.kume@hotmail.com" -q > /dev/null
+  echo "  ✅ Generate succeed"
+fi
