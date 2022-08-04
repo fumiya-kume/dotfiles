91aa28c65bf7d33aeb4587ca37578fe17b9e9fd8
kuu(Fumiya Kume) <fumiya.kume@hotmail.com>
2022-08-04 10:30:21 +0900

    Try link for Golang with Homebrew

--- jobs/setup_homebrew.sh
+++ jobs/setup_homebrew.sh
@@ -1,2 +1,3 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew link go
 brew tap homebrew/bundle > /dev/null && brew upgrade > /dev/null && brew bundle install --global
