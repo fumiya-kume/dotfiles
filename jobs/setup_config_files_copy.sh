7fb201ffef8b1565ceaf15d4386630fa13382a4b
kuu(Fumiya Kume) <fumiya.kume@hotmail.com>
2022-08-04 18:20:38 +0900

    Update setup_config_files_copy.sh

--- jobs/setup_config_files_copy.sh
+++ jobs/setup_config_files_copy.sh
@@ -9,6 +9,7 @@ ln -s "$(pwd)/config/.Brewfile" ~/.Brewfile
 ln -s "$(pwd)/config/.hammerspoon" ~/.hammerspoon
 ln -s "$(pwd)/config/.tmux/" ~/.tmux
 
+ln -fs "$(pwd)/config/.ssh/config" ~/.ssh/config
 git config --global include.path "~/.gitconfig.remote"
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
