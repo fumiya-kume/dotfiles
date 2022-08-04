16085c9372948f142e4d98326a1d0163bb798ebc
kuu(Fumiya Kume) <fumiya.kume@hotmail.com>
2022-08-05 05:46:52 +0900

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 mkdir ~/ghq
 git clone https://github.com/fumiya-kume/dotfiles.git ~/ghq/dotfiles > /dev/null
-sh ~/ghq/dotfiles/workflow/full_install.sh
+cd ~/ghq/dotfiles && sh ./workflow/full_install.sh
