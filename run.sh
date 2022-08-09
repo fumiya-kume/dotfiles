c6aeb0f9f6f32ff41fda6d011f96ba13b52e4a09
kuu(Fumiya Kume) <fumiya.kume@hotmail.com>
2022-08-09 13:57:40 +0900

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,30 @@
+/bin/bash
+
+cd ~/ghq/dotfiles/run.sh
+
+echo "✅ Install Xcode"
+sudo xcode-select --install > /dev/null
+
+echo "✅ Copy files"
+sh ./jobs/setup_config_files_copy.sh
+
+echo "✅ Install Homebrew"
+. ./jobs/setup_homebrew.sh
+
+echo "✅ Set fish as default shell"
+. ./jobs/setup_fish.sh
+
+echo "✅ Set Mac defaults"
+. ./jobs/setup_mac_defaults.sh
+
+echo "✅ Setup gn"
+. ./jobs/setup_gn.sh
+
+echo "✅ Setup SSH for Github"
+. ./jobs/setup_ssh_github.sh
+
+echo "✅ Setup GPG for Github"
+. ./jobs/setup_gpg.sh
+
+echo "✅ Test configuration"
+. ./jobs/test.sh
