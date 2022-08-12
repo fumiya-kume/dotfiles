002cbf642d7198d53434d9686cc5a01f4ffba80e
kuu(Fumiya Kume) <fumiya.kume@hotmail.com>
2022-08-12 10:50:41 +0900

    Delete setup_gn.sh

--- jobs/setup_gn.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-mkdir ~/tmp
-git clone https://gn.googlesource.com/gn ~/tmp/gn -q
-python ~/tmp/gn/build/gen.py
-ninja -C ~/tmp/gn/out > /dev/null
-
-mkdir -p ~/.bin
-cp ~/tmp/gn/out/gn ~/.bin
-rm -rf ~/tmp
