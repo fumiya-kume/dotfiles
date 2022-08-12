ac9b1b386d07494392593c9fee41283ad5ca90f1
kuu(Fumiya Kume) <fumiya.kume@hotmail.com>
2022-08-12 13:06:28 +0900

    Delete setup_homebrew.sh (#72)
    * Delete setup_homebrew.sh
    
    * Update run.sh
    
    * Update run.sh
    
    * Delete setup_ssh_github.sh
    
    * Delete test.sh
    
    * Update run.sh
    
    * Update run.sh
    
    * Delete setup_gn.sh
    
    * Update run.sh
    
    * Delete setup_config_files_copy.sh
    
    * Update run.sh
    
    * Update run.sh
    
    * Update run.sh

--- jobs/setup_ssh_github.sh
+++
@@ -1,6 +0,0 @@
-if [ -f "$1" ]; then
-  echo "  🚀 Generate SSH key for Github skipped"
-else 
-  ssh-keygen -t ed25519 -f ~/.ssh/github -N '' -C "fumiya.kume@hotmail.com" -q > /dev/null
-  echo "✅ Generate succeed"
-fi
--- run.sh
+++ run.sh
@@ -4,10 +4,30 @@ echo "✅ Install Xcode"
 sudo xcode-select --install > /dev/null
 
 echo "✅ Copy files"
-sh ./jobs/setup_config_files_copy.sh
+rm -rf ~/.config
+ln -fs "$(pwd)/config/.config/" ~
+ln -fs "$(pwd)/config/.gnupg/" ~
+ln -fs "$(pwd)/config/.tigrc" ~/.tigrc
+ln -fs "$(pwd)/config/.gitconfig.remote" ~/.gitconfig.remote
+ln -fs "$(pwd)/config/.gitmodules" ~/.gitmodules
+ln -fs "$(pwd)/config/.gitignore" ~/.gitignore
+ln -fs "$(pwd)/config/.Brewfile" ~/.Brewfile
+ln -fs "$(pwd)/config/.hammerspoon" ~/.hammerspoon
+ln -fs "$(pwd)/config/.tmux/" ~/.tmux
+ln -fs "$(pwd)/config/.ssh/config" ~/.ssh/config
+
+mkdir -p "~/Library/Application\ Support/Code/"
+ln -nfs "$(pwd)/config/vscode/User/" "~/Library/Application\ Support/Code/"
+
+git config --global include.path "~/.gitconfig.remote"
+git config --global core.excludesfile "~/.gitignore"
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim -q' > /dev/null
 
 echo "✅ Install Homebrew"
-. ./jobs/setup_homebrew.sh
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" > /dev/null
+brew tap homebrew/bundle > /dev/null && brew bundle install --global -q
 
 echo "✅ Set fish"
 if [[ `uname -m` == 'arm64' ]]; then
@@ -22,13 +42,18 @@ echo "✅ Set Mac defaults"
 . ./jobs/setup_mac_defaults.sh
 
 echo "✅ Setup gn"
-. ./jobs/setup_gn.sh
+mkdir ~/tmp
+git clone https://gn.googlesource.com/gn ~/tmp/gn -q
+python ~/tmp/gn/build/gen.py && ninja -C ~/tmp/gn/out > /dev/null
+mkdir -p ~/.bin && cp ~/tmp/gn/out/gn ~/.bin && rm -rf ~/tmp
 
 echo "✅ Setup SSH for Github"
-. ./jobs/setup_ssh_github.sh
+if [ -f ~/.ssh/github ]; then
+  echo "  🚀 Generate SSH key for Github skipped"
+else 
+  ssh-keygen -t ed25519 -f ~/.ssh/github -N '' -C "fumiya.kume@hotmail.com" -q > /dev/null
+  echo "✅ Generate succeed"
+fi
 
 echo "✅ Setup GPG for Github"
 . ./jobs/setup_gpg.sh
-
-echo "✅ Test configuration"
-. ./jobs/test.sh
