9d7bb8b04679dee8b410b2fa5feb1013c558d11f
kuu(Fumiya Kume) <fumiya.kume@hotmail.com>
2022-08-12 10:53:19 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -14,11 +14,11 @@ ln -fs "$(pwd)/config/.gitignore" ~/.gitignore
 ln -fs "$(pwd)/config/.Brewfile" ~/.Brewfile
 ln -fs "$(pwd)/config/.hammerspoon" ~/.hammerspoon
 ln -fs "$(pwd)/config/.tmux/" ~/.tmux
+ln -fs "$(pwd)/config/.ssh/config" ~/.ssh/config
 
 mkdir -p "~/Library/Application\ Support/Code/"
 ln -nfs "$(pwd)/config/vscode/User/" "~/Library/Application\ Support/Code/"
 
-ln -fs "$(pwd)/config/.ssh/config" ~/.ssh/config
 git config --global include.path "~/.gitconfig.remote"
 git config --global core.excludesfile "~/.gitignore"
 
