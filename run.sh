ab570f503ae0c27ccd3bbf60f3af15b522859ba4
kuu(Fumiya Kume) <fumiya.kume@hotmail.com>
2022-08-12 10:50:58 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,26 @@ echo "✅ Install Xcode"
 sudo xcode-select --install > /dev/null
 
 echo "✅ Copy files"
-sh ./jobs/setup_config_files_copy.sh
+rm -rf ~/.config
+ln -fs "$(pwd)/config/.config/" ~
+ln -fs "$(pwd)/config/.gnupg/" ~
+ln -fs "$(pwd)/config/.tigrc" ~/.tigrc
+ln -fs "$(pwd)/config/.gitconfig.remote" ~/.gitconfig.remote
+ln -fs "$(pwd)/config/.gitmodules" ~/.gitmodules
+ln -fs "$(pwd)/config/.gitignore" ~/.gitignore
+ln -fs "$(pwd)/config/.Brewfile" ~/.Brewfile
+ln -fs "$(pwd)/config/.hammerspoon" ~/.hammerspoon
+ln -fs "$(pwd)/config/.tmux/" ~/.tmux
+
+mkdir -p "~/Library/Application\ Support/Code/"
+ln -nfs "$(pwd)/config/vscode/User/" "~/Library/Application\ Support/Code/"
+
+ln -fs "$(pwd)/config/.ssh/config" ~/.ssh/config
+git config --global include.path "~/.gitconfig.remote"
+git config --global core.excludesfile "~/.gitignore"
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim -q' > /dev/null
 
 echo "✅ Install Homebrew"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" > /dev/null
