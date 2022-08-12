531709a425331f1dbaccbe147291c9181c96105c
kuu(Fumiya Kume) <fumiya.kume@hotmail.com>
2022-08-12 13:08:56 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -57,8 +57,6 @@ fi
 
 echo "✅ Setup GPG for Github"
 gpg --full-gen-key -q --batch "$(pwd)/config/gpg-key.conf" > /dev/null
-
 KEY_ID=$(gpg --list-secret-keys --with-colons | awk -F: '$1 == "sec" {print $5}' | tail -n 1)
-
 echo $KEY_ID | xargs gpg --armor --export 
 echo "git config --global user.signingkey $KEY_ID"
