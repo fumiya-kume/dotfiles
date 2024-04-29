d52eb2640440a5b5cc2a6111e97cded1cfa8e69a
kuu(Fumiya Kume) <fumiya.kume@hotmail.com>
2024-04-29 19:35:36 +0900

    Get fish path from command

--- run.sh
+++ run.sh
@@ -32,13 +32,8 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 brew tap homebrew/bundle > /dev/null && brew bundle install --global -q
 
 echo "✅ Set fish"
-if [[ `uname -m` == 'arm64' ]]; then
-   echo /opt/homebrew/bin/fish | sudo tee -a /etc/shells 2>&1 > /dev/null
-   sudo chsh -s /opt/homebrew/bin/fish > /dev/null
-else
-   echo /usr/local/bin/fish | sudo tee -a /etc/shells 2>&1 > /dev/null
-   sudo chsh -s /opt/homebrew/bin/fish > /dev/null
-fi
+echo $(which fish) | sudo tee -a /etc/shells 2>&1 > /dev/null
+sudo chsh -s /opt/homebrew/bin/fish > /dev/null
 
 echo "✅ Set Mac defaults"
 . ./command/mac_defaults.sh
